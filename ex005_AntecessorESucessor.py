336614b2e89e47ff280d6e932364726e9722f8d8
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-05 23:23:33 -0300

    feat: add ex005_AntecessorESucessor.py

---
+++ ex005_AntecessorESucessor.py
@@ -0,0 +1,4 @@
+numero = int(input('Digite um número'))
+sucessor = int(numero + 1)
+antecessor = int(numero - 1)
+print('O número digitado foi {}, o sucessor dele é {}, e o antecessor dele é {}'.format(numero, sucessor, antecessor))
