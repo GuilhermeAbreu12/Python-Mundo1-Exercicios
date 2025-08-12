c4a9641dcbe4a9392a967d4ba4f6ba9307d3fd17
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-08-12 20:30:54 -0300

    docs: add description of exercise 022

--- ex022_ManipuladorDeNome.py
+++ ex022_ManipuladorDeNome.py
@@ -1,4 +1,9 @@
-'''Ex.022'''
+# Aula 09: Manipulando Texto
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# - O nome com todas as letras maiúsculas;
+# - O nome com todas minúsculas;
+# - Quantas letras ao todo (sem considerar espaços);
+# - Quantas letras tem o primeiro nome.
 nome = str(input('Digite seu nome completo: ')).strip()
 caracteres = nome.split()
 carac2 = len(caracteres)
